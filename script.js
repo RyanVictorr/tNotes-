18f0d8ae19cd0b596b5adfaaef30f970bad34d3b
DanielReisdC <49843024+DanielReisdC@users.noreply.github.com>
2023-05-08 17:04:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
 function acao() {
-  let modal= document.querySelector('.modal')
-   modal.style.display="block";
-    
-}
-function fechar(){
-    let modal= document.querySelector('.modal')
-   modal.style.display="none";
-}
-let novaTarefa = document.querySelector('novaTarefa');
-let btnAddTarefa = document.querySelector('btnAddTarefa');
-let lisTarefa = document.querySelector('listaTarefa');
+   let modal= document.querySelector('.modal')
+    modal.style.display="block";
+     
+ }
+ function fechar(){
+     let modal= document.querySelector('.modal')
+    modal.style.display="none";
+ }
+ let novaTarefa = document.querySelector('#novaTarefa');
+ let btnAddTarefa = document.querySelector('#btnAddTarefa');
+ let listaTarefa = document.querySelector('#listaTarefa');
+ 
+ novaTarefa.addEventListener('keypress', (e)=>{
+    if(e.keycode==13){
+    let tarefa ={
+       nome: novaTarefa.value, 
+       id:gerarId(),
+    }
+       adicionarTarefa('tarefa');
+ }});
+ 
+ 
+ btnAddTarefa.addEventListener('click',(e)=>{
+    let tarefa={
+       nome:novaTarefa.value,
+       id:gerarId(),
+    }
+    adicionarTarefa('tarefa');
+   
+ });
+ 
+ function gerarId(){
+    return Math.floor(Math.random()*300);
+ 
+ }
+ function adicionarTarefa(tarefa){
+    let li = criarTagLI(tarefa);
+    listaTarefa.appendChild(li);
+    novaTarefa.value='';
+ }
+ 
+ function criarTagLI(tarefa){
+    let li=document.createElement('li');
+    let span = document.createElement('span')
+    span.classList.add('textoTarefa');
+    span.innerHTML = tarefa.nome;
+ 
+    let div = document.createElement('div');
+    let btnEditar = document.createElement('button');
+    btnEditar.classList.add('btnAcao');
+    btnEditar.innerHTML='<i class="fa fa-pencil"></i>';
+    let btnExcluir = document.createElement('button');
+    btnExcluir.classList.add('btnAcao');
+    btnExcluir.innerHTML='<i class="fa fa-trash"></i>';
+ 
+    div.appendChild(btnEditar);
+    div.appendChild(btnExcluir);
 
-novaTarefa.addEventListener('keypress', (e)=>{
-   if(e.keycode==13){
-   let tarefa ={
-      nome: novaTarefa.value, 
-      id:gerarId(),
-   }
-      adicionarTarefa('tarefa');
-}});
-
-
-btnAddTarefa.addEventListener('click',(e)=>{
-   let tarefa={
-      nome:novaTarefa.value,
-      id:gerarId(),
-   }
-   adicionarTarefa('tarefa');
-  
-});
-
-function gerarId(){
-   return Math.floar(Math.random()*300);
-
-}
-function adicionarTarefa(tarefa){
-   let li = criarTagLi(tarefa);
-   lisTarefa.appendChild(li);
-   novaTarefa.value='';
-}
-
-function criarTagLi(tarefa){
-   let li=document.createElement('li');
-   let span = document.createElement('span')
-   span.classList.add('textoTarefa');
-   span.innerHTML = tarefa.nome;
-
-   let div = document.createElement('div');
-   let btnEditar = document.createElement('button');
-   btnEditar.classList.add('btnAcao');
-   btnEditar.innerHTML='<i class="fa fa-pencil"></i>'
-   let btnExcluir = document.createElement('button');
-   btnExcluir.classList.add('btnAcao');
-   btnExcluir.innerHTML='<i class="fa fa-trash"></i>'
-
-   div.appendChild(btnEditar);
-   div.appendChild(btnExcluir);
-   li.appendChild(span);
-   li.appendChild(div);
-   return li;
-}
+    li.appendChild(span);
+    li.appendChild(div);
+    return li;
+ }
